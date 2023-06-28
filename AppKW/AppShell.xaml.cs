299de465de459aa8a0270685890a462a589f686b
Cristian <clr.3009@gmail.com>
2023-06-28 11:37:55 -0600

    Mostrar el StartPage como primera pantalla

--- AppKW/AppShell.xaml.cs
+++ AppKW/AppShell.xaml.cs
@@ -1,8 +1,4 @@
-﻿using AppKW.ViewModels;
-using AppKW.Views;
-using System;
-using System.Collections.Generic;
-using Xamarin.Essentials;
+﻿using System;
 using Xamarin.Forms;
 
 namespace AppKW
@@ -12,27 +8,11 @@ namespace AppKW
         public AppShell()
         {
             InitializeComponent();
-            Routing.RegisterRoute("user1", typeof(LoginPage));
-            Routing.RegisterRoute("user2", typeof(LoginPage));
-            RolInvitado();
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("//LoginPage");
-        }
-
-        private async void RolInvitado()
-        {
-            //Validar tipo de usuario 
-            string role = await SecureStorage.GetAsync("role");
-
-            if (string.IsNullOrEmpty(role))
-            {
-                MessagingCenter.Send<AppShell>(this,
-                    role = "Invitado"
-                );
-            }
         }
     }
 }
