7c327dc73499a81578857df1d993ad884f8593de
Cristian <clr.3009@gmail.com>
2023-06-28 12:28:19 -0600

    Validar formulario de registro

--- AppKW/Views/Registro.xaml.cs
+++ AppKW/Views/Registro.xaml.cs
@@ -1,46 +1,30 @@
-﻿using AppKW.ViewModels;
-using System;
+﻿using System;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using String = System.String;
 
 namespace AppKW.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Registro : ContentPage
     {
-        UsuarioRepositorio _userRepository = new UsuarioRepositorio();
         public Registro()
         {
             InitializeComponent();
-            NavigationPage.SetHasNavigationBar(this, false);
         }
-        public async void ButtonRegister_Clicked(object sender, EventArgs e)
+        public async void Register(object sender, EventArgs e)
         {
-            bool isValidForm = await validateRegister(TxtName.Text, TxtLastName.Text, TxtEmail.Text, TxtPassword.Text, TxtConfirmPass.Text);
+            bool isValid = await validateRegister(TxtName.Text, TxtLastname.Text, TxtEmail.Text, TxtPassword.Text, TxtConfirmPassword.Text);
 
-            if (isValidForm)
+            if (isValid)
             {
-                try
-                {
-                    await _userRepository.register(TxtName.Text.Trim(), TxtLastName.Text.Trim(), TxtEmail.Text.Trim(), TxtPassword.Text.Trim());
-                    await DisplayAlert("Registro exitoso", "Se envió un enlace de verificación a tu correo", "Aceptar");
-                    await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
-                }
-                catch (System.Exception ex)
-                {
-                    if (ex.Message.Contains("EMAIL_EXISTS"))
-                    {
-                        await DisplayAlert("Error", "Este nombre de usuario ya está en uso", "Aceptar");
-                        clearForm();
-                    }
-                    else
-                    {
-                        await DisplayAlert("Error", "Algo salió mal, inténtalo más tarde", "Aceptar");
-                    }
-                }
+                string name = TxtName.Text.Trim();
+                string lastname = TxtLastname.Text.Trim();
+                string email = TxtEmail.Text.Trim();
+                string password = TxtPassword.Text.Trim();
+
+                Console.WriteLine($"{name} - {lastname} - {email} - {password}");
             }
         }
         private void imageButtonFacebook(object sender, EventArgs e)
@@ -50,36 +34,28 @@ namespace AppKW.Views
         private void imageButtonTwitter(object sender, EventArgs e)
         {
             Launcher.OpenAsync(new System.Uri("https://twitter.com/KenworthdelEste"));
-        }
-        private void clearForm()
-        {
-            TxtName.Text = string.Empty;
-            TxtLastName.Text = string.Empty;
-            TxtEmail.Text = string.Empty;
-            TxtPassword.Text = string.Empty;
-            TxtConfirmPass.Text = string.Empty;
-        }
+        }        
         private async Task<bool> validateRegister(string name, string lastname, string email, string password, string confirmPassword)
         {
-            if (String.IsNullOrEmpty(name))
+            if (string.IsNullOrEmpty(name))
             {
                 await DisplayAlert("Error", "Ingresa tu nombre", "Aceptar");
                 return false;
             }
 
-            if (String.IsNullOrEmpty(lastname))
+            if (string.IsNullOrEmpty(lastname))
             {
                 await DisplayAlert("Error", "Ingresa tu apellido", "Aceptar");
                 return false;
             }
 
-            if (String.IsNullOrEmpty(email))
+            if (string.IsNullOrEmpty(email))
             {
                 await DisplayAlert("Error", "Ingresa una dirección de correo electrónico", "Aceptar");
                 return false;
             }
 
-            if (String.IsNullOrEmpty(password))
+            if (string.IsNullOrEmpty(password))
             {
                 await DisplayAlert("Error", "Ingresa una contraseña", "Aceptar");
                 return false;
@@ -91,7 +67,7 @@ namespace AppKW.Views
                 return false;
             }
 
-            if (String.IsNullOrEmpty(confirmPassword))
+            if (string.IsNullOrEmpty(confirmPassword))
             {
                 await DisplayAlert("Error", "Confirma tu contraseña", "Aceptar");
                 return false;
