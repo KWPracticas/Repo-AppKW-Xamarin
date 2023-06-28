06004954f8eddb6ea1dc7b62f6394d017707c0ba
Cristian <clr.3009@gmail.com>
2023-06-28 11:30:55 -0600

    Limpieza del App

--- AppKW/App.xaml.cs
+++ AppKW/App.xaml.cs
@@ -1,12 +1,5 @@
 ﻿using AppKW.Services;
-using AppKW.ViewModels;
-using AppKW.Views;
-using System;
-using System.Threading.Tasks;
-using Xamarin.Essentials;
 using Xamarin.Forms;
-using Xamarin.Forms.BackgroundVideoView;
-using Xamarin.Forms.Xaml;
 
 namespace AppKW
 {
@@ -20,23 +13,6 @@ namespace AppKW
             DependencyService.Register<MockDataStore>();
             MainPage = new AppShell();
             
-        }
-
-        protected override void OnStart()
-        {
-            //await _userRepository.validateToken();
-
-            base.OnStart();
-        }
-
-        protected override void OnSleep()
-        {
-            //SecureStorage.RemoveAll();
-        }
-
-        protected override void OnResume()
-        {
-
         }
     }
 }
