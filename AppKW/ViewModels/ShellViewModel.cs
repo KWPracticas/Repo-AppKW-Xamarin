d5a115394dadb831b1fb9c428f8b140ecdfd42db
Cristian <clr.3009@gmail.com>
2023-06-29 22:45:39 -0600

    Cerrar o iniciar sesion dependiendo el usuario

--- AppKW/ViewModels/ShellViewModel.cs
+++ AppKW/ViewModels/ShellViewModel.cs
@@ -9,21 +9,36 @@ namespace AppKW.ViewModels
         public bool isEmployee;
         public bool isGuest;
 
+        public string isTitle;
+        public string icon;
+
         public bool IsRegularUser { get => isRegularUser; set => SetProperty(ref isRegularUser, value); }
         public bool IsEmployee { get => isEmployee; set => SetProperty(ref isEmployee, value); }
         public bool IsGuest { get => isGuest; set => SetProperty(ref isGuest, value); }
+
+        public string IsTitle { get => isTitle; set => SetProperty(ref isTitle, value); }
+        public string Icon { get => icon; set => SetProperty(ref icon, value); }
+
         public ShellViewModel() 
         {
             MessagingCenter.Subscribe<LoginPage>(this, "isRegularUser", (sender) =>
             {
                 IsRegularUser = true;
                 IsEmployee = false;
+                IsGuest = false;
+
+                IsTitle = "Cerrar sesión";
+                Icon = "logout.png";
             });
 
             MessagingCenter.Subscribe<LoginPage>(this, "isEmployee", (sender) =>
             {
                 IsEmployee = true;
                 IsRegularUser = false;
+                IsGuest = false;
+
+                IsTitle = "Cerrar sesión";
+                Icon = "logout.png";
             });
 
             MessagingCenter.Subscribe<StartPage>(this, "isGuest", (sender) =>
@@ -31,6 +46,9 @@ namespace AppKW.ViewModels
                 IsGuest = true;
                 IsRegularUser = false;
                 IsEmployee = false;
+
+                IsTitle = "Iniciar sesión";
+                Icon = "login.png";
             });
         }
     }
